1602bca676cc8f9d63ea4ccd6892e8cd0986adfe
vanthiel.moritz <vanthiel.moritz@E211-18-12>
2023-11-29 09:43:43 +0100

    Projektdateien hinzufügen.

---
+++ Subnetzrechner/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subnetzrechner
+{
+    public partial class ViewNetzwerkRechner : Form
+    {
+        public ViewNetzwerkRechner()
+        {
+            InitializeComponent();
+        }
+
+        private void buttondeine_IP_Click(object sender, EventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                   textBoxIP_Adresse.Text = ip.ToString();
+                }
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxAnzahlHosts.Clear();
+            textBoxbis.Clear();
+            textBoxBroadcast.Clear();
+            textBoxHostIPSvon.Clear();
+            textBoxInverseNetzwerkmask.Clear();
+            textBoxIP_Adresse.Clear();
+            textBoxNetzadresse.Clear();
+            textBoxNetzwerkmask.Clear();
+            comboBoxCIDR_Suffix.Items.Clear();
+            comboBoxCIDR_Suffix.ResetText();
+        }
+    }
+}
